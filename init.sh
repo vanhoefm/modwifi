c7de0469164ff9c349dec0ad7a11c21b01006ffd
Mathy <vanhoefm@users.noreply.github.com>
2015-01-15 13:45:16 -0500

    Allow initialization to be easily interrupted

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap "killall background" EXIT
 git clone -b master   git@github.com:vanhoefm/modwifi.git           modwifi   &
 git clone -b research git@github.com:vanhoefm/modwifi-linux.git     linux     &
 git clone -b research git@github.com:vanhoefm/modwifi-ath9k-htc.git ath9k-htc &
