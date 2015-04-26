cd6be4c9c582e5f9a095ef817dbeaa2c96458337
Mathy <vanhoefm@users.noreply.github.com>
2015-04-26 11:57:50 -0400

    init.sh: use public protocol as default

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 trap "killall background" EXIT
-git clone -b master   git@github.com:vanhoefm/modwifi.git           modwifi   &
-git clone -b research git@github.com:vanhoefm/modwifi-linux.git     linux     &
-git clone -b research git@github.com:vanhoefm/modwifi-ath9k-htc.git ath9k-htc &
-git clone -b research git@github.com:vanhoefm/modwifi-backports.git backports &
-git clone -b master   git@github.com:vanhoefm/modwifi-tools.git     tools     &
+
+PROTOCOL="https://github.com/"
+if [[ $# -eq 1 ]]; then
+    PROTOCOL="git@github.com:"
+fi
+
+git clone -b master   ${PROTOCOL}vanhoefm/modwifi.git           modwifi   &
+git clone -b research ${PROTOCOL}vanhoefm/modwifi-linux.git     linux     &
+git clone -b research ${PROTOCOL}vanhoefm/modwifi-ath9k-htc.git ath9k-htc &
+git clone -b research ${PROTOCOL}vanhoefm/modwifi-backports.git backports &
+git clone -b master   ${PROTOCOL}vanhoefm/modwifi-tools.git     tools     &
 wait
